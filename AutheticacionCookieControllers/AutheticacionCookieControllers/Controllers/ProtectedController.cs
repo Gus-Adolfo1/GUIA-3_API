78c336bd2c49e4dde03829cc63103970645f5a2b
Gustavo Munoz <softwareadolfo@gmail.com>
2023-09-14 16:29:29 -0600

    ejercico de ejemplo guia 3 terminado

---
+++ AutheticacionCookieControllers/AutheticacionCookieControllers/Controllers/ProtectedController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutheticacionCookieControllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProtectedController : ControllerBase
+    {
+        static List<object> data = new List<object>();
+        // GET: api/<ProtectedController>
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            return data;
+        }
+
+        // POST api/<ProtectedController>
+        [HttpPost]
+        public IActionResult Post(string name, string lastname)
+        {
+            data.Add(new { name, lastname });
+            return Ok();
+        }
+    }
+}
--- AutheticacionCookieControllers/AutheticacionCookieControllers/Program.cs
+++ AutheticacionCookieControllers/AutheticacionCookieControllers/Program.cs
@@ -1,3 +1,9 @@
+
+
+//add the name to authentica using cookies 
+using Microsoft.AspNetCore.Authentication.Cookies;
+//add namespace to use JSON
+using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//configure servuces to authenticate using cookies
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        //set parameters for non-authorized users
+        options.ReturnUrlParameter = "unauthorized";
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = context =>
+            {
+                //change the code to non-authorized 
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                context.Response.ContentType = "application/json";
+                var message = new
+                {
+                    error = "Unauthorized",
+                    message = "Must be log in to access this resource."
+                };
+                //Serealize the object "message" in Json
+                var jsonMessage = JsonSerializer.Serialize(message);
+                // wrirte the message in the HTTP RESPONSE
+                return context.Response.WriteAsync(jsonMessage);
+
+            }
+        };
+
+    });
 
 var app = builder.Build();
 
